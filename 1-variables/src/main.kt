0615e8274ccae2b5296276f1542650631d189679
BBarisKilic <bulentbariskilic@gmail.com>
2021-03-18 16:55:16 +0300

    Variables and Constants added.

---
+++ 1-variables/src/main.kt
@@ -0,0 +1,10 @@
+fun main(args : Array<String>){
+    var a = 5
+    var b = 10
+
+    println(a * b)
+
+    var age = 10
+
+    println(age /5*8)
+}
---
+++ 2-constants/src/main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val a = 10
+    //a = 10 => Val cannot be reassigned.
+    val pi = 3.14
+
+    val result = a + pi
+
+    println(result)
+}
