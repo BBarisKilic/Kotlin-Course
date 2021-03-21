7357820c13a8e2a3633f738771686c66fcdbd1f6
BBarisKilic <bulentbariskilic@gmail.com>
2021-03-21 18:24:39 +0300

    DataTypeChange added.

--- 3-dataTypes/src/main.kt
+++ 3-dataTypes/src/main.kt
@@ -3,11 +3,13 @@ fun main() {
     println("-----Integer-----")
     val myInteger : Int = 5
     println(myInteger / 2) //result = 2
+
     //Long
     println("-----Long-----")
     var myLong : Long = 100
     myLong = 30000000000000
     println(myLong/2)
+
     //Double and Float
     println("-----Float-----")
     val pi : Float = 3.14f
@@ -15,9 +17,24 @@ fun main() {
     println("-----Double-----")
     val myDouble = 5.0 // Default double
     println(myDouble / 2) //result = 2.5
+
     //String
     println("-----String-----")
     val myString : String = "Hello Kotlin"
     println(myString)
     println("Length: ${myString.length}")
+    val firstName = "Barış"
+    val lastName = "Kılıç"
+    val name = firstName + " " + lastName
+    println(name)
+    val newString : String = "10"
+    val anotherNewString : String = "20"
+    println(newString + anotherNewString) //result = 1020
+
+    //Boolean
+    println("-----Boolean-----")
+    var myBoolean : Boolean = true
+    myBoolean = false
+    println(myBoolean)
+    println(3<5)
 }
---
+++ 4-dataTypeChange/src/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    //Data type change (int to long)
+    val myInt = 10
+    val myLong = myInt.toLong()
+    println(myLong)
+
+    //Data type change (string to int)
+    val userInput = "50"
+    val userInt = userInput.toInt()
+    println(userInt/2);
+}
